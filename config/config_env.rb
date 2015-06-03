27d5848432242738dd4e2ca3e24aa87a6607059b
tnfontenelle <tnfontenelle@hotmail.com>
2015-06-03 11:58:27 +0800

    Deleted file

--- config/config_env.rb
+++
@@ -1,9 +0,0 @@
-config_env :development, :test do
-  set 'DB_KEY', "EnxXKNw7g-XwXhAsmP3XI7l00cfpNge5o9NHli8OO94="
-  set 'MSG_KEY', "vjzUNM1pM18_QX3eHljK92GiTiL22gIYwL6d9TKSjKE="
-end
-
-config_env :production do
-  set 'DB_KEY', "EnxXKNw7g-XwXhAsmP3XI7l00cfpNge5o9NHli8OO94="
-  set 'MSG_KEY', "vjzUNM1pM18_QX3eHljK92GiTiL22gIYwL6d9TKSjKE="
-end
